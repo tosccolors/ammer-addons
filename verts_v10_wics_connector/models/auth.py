2a6179f637207b7bf051262b17c414b1c7841560
Willem Hulshof <w.hulshof@magnus.nl>
2019-12-19 16:32:07 +0100

    task iso task_json

--- verts_v10_wics_connector/models/auth.py
+++ verts_v10_wics_connector/models/auth.py
@@ -244,10 +244,10 @@ class StockIPicking(models.Model):
             if not config.endpoint or not config.auth_key or not config.secret_key:
                 return "Incomplete Wics order interface configuration for order. Need input on endpoint, auth_key and " \
                        "secret_key. "
-            print config.endpoint, task_json
+            print config.endpoint, task
             # created dictionary using of post method create order on wics server and order is created on wics server
             try:
-                resp = requests.post(config.endpoint, auth=auth, json=task_json,
+                resp = requests.post(config.endpoint, auth=auth, json=task,
                                  headers=headers)
 #            if resp.ok:
                 # if data is none or failure message
